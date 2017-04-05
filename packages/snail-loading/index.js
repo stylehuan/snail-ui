5cfe087983bbd7930431d9c55d5fe004e149e2c8
stylehuan <fhuanweb@gmail.com>
2017-04-05 15:27:41 +0800

    update loading manager

--- packages/snail-loading/index.js
+++ packages/snail-loading/index.js
@@ -3,5 +3,41 @@
  * @author  stylehuan
  * @create  2017-04-05 09:50
  */
-export default from "./src/loading.vue"
+import LoadingConstructor from "./src/loading.vue"
+let _instance;
+let plugin = {
+    install(vue, options){
+        const Loading = vue.extend(LoadingConstructor);
+        if (!_instance) {
+            _instance = new Loading({
+                el: document.createElement('div')
+            });
+            document.body.appendChild(_instance.$el);
+        }
+        let loading = {
+            show(option){
+                if (typeof options === 'string') {
+                    _instance.text = options
+                }
+                _instance.show = true
+            },
+            hide () {
+                _instance.show = false
+            }
+        };
+        if (!vue.$snail) {
+            vue.$snail = {
+                loading
+            }
+        } else {
+            vue.$snail.loading = loading
+        }
+        vue.mixin({
+            created: function () {
+                this.$loading = vue.$snail.loading
+            }
+        })
+    }
+}
+export default plugin;
  
